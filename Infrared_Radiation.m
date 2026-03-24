b1ed580fcef58f3baa5a248a125f1a019307a134
Mahmoud-Sci <m.diab@aun.edu.eg>
2026-03-24 22:29:26 +0200

    Update Infrared_Radiation.m

--- Infrared_Radiation.m
+++ Infrared_Radiation.m
@@ -1,4 +1,4 @@
-function IR = Infrared_Radiation(lighting,Amp_temp)
+function IR_norm = Infrared_Radiation(lighting,Amp_temp)
 h=6.62607015e-34;
 c=2.998e8;      %M/S
 kb=(1.380649*10^(-23)) * (9/5); %by J/F
@@ -11,7 +11,7 @@ c2=(h*c)/(lam*kb);
 J=(Amp_temp*lighting);
 B=(c1/lam^5)*(1/(exp(c2/Tp)-1));
 IR=J*B;
-IR=1+(IR-0)*9/(745.4497-0);
+IR_norm=1+(IR-0)*9/(745.4497-0);
 end
 
 
